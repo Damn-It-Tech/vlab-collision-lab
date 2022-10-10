59b7d3134c7d9643fdbb9b9c44bdf825ba85eaad
Mehul Jain <mj@Mehuls-MacBook-Pro.local>
2022-10-10 22:00:07 +0530

    title updated

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Collision Lab',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
